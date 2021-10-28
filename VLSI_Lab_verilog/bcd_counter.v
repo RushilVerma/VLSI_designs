e9ea3940e49578563b05d1ae3a16d8bb9f00bd0e
Rushil Verma <44629500+RushilVerma@users.noreply.github.com>
2021-10-28 21:17:23 +0530

    Verilog designs

---
+++ VLSI_Lab_verilog/bcd_counter.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:16:35 10/22/2020 
+// Design Name: 
+// Module Name:    bcd_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module bcd_counter(clk, reset, dout);
+	output [3:0] dout;
+	reg [3:0] dout;
+	
+	input clk;
+	wire clk;
+	input reset;
+	wire reset;
+
+	initial dout<=0;
+	 
+	always@(posedge(clk)) begin
+	
+		if(reset==1'b1) dout<=0;
+		
+		else if(dout<9) dout<=dout+1;
+		
+		else if(dout==9) dout<=0;
+	end
+
+endmodule
